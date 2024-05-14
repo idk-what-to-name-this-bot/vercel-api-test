0884c2451507c4912861d615b16ecf5bec29b183
AiverAiva <43096905+AiverAiva@users.noreply.github.com>
2024-05-14 15:14:02 +0800

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,8 @@ import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 @Module({
   imports: [
