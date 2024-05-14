5b268529101796953631efbba391e1660327d9e8
AiverAiva <43096905+AiverAiva@users.noreply.github.com>
2024-05-14 14:33:31 +0800

    aaa

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,43 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    MongooseModule.forRoot(
+      process.env.MONGODB_URI,
+    ),
     ConfigModule.forRoot({
       envFilePath: '.env',
     }),
-    MongooseModule.forRoot(
-      process.env.MONGODB_HOST,
-    ),
-    // ConfigModule.forRoot(),
   ],
   controllers: [AppController],
-  providers: [],
+  providers: [AppService],
 })
 export class AppModule {}
-
-// @Module({
-//   imports: [
-//     PassportModule.register({ session: true }),
-//     TypeOrmModule.forRoot({
-//       type: 'mongodb',
-//       host: process.env.MYSQL_DB_HOST,
-//       port: 3306,
-//       username: process.env.MYSQL_DB_USERNAME,
-//       password: process.env.MYSQL_DB_PASSWORD,
-//       database: process.env.MYSQL_DB_DATABASE,
-//       synchronize: true,
-//       entities: entities,
-//     }),
-//     AuthModule,
-//     UserModule,
-//     DiscordModule,
-//     GuildsModule,
-//     WebSocketModule,
-//   ],
-//   controllers: [],
-//   providers: [],
-// })
-// export class AppModule {}
