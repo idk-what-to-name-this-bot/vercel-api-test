624e3aadbc262068210ef46774d6ebcc4cb95f5b
AiverAiva <43096905+AiverAiva@users.noreply.github.com>
2024-05-14 13:22:18 +0800

    aaaa

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
-import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
