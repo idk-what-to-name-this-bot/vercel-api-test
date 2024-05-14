c3009a10781c9dad9e5cf43fa6b9d8e3dc3676eb
AiverAiva <43096905+AiverAiva@users.noreply.github.com>
2024-05-14 14:53:57 +0800

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,7 +11,7 @@ import { ConfigModule } from '@nestjs/config';
     UsersModule,
     AuthModule,
     MongooseModule.forRoot(
-      process.env.MONGODB_URI,
+      'mongodb+srv://leduc1901:melody19@kylecluster.abjzu.mongodb.net/?retryWrites=true&w=majority',
     ),
     ConfigModule.forRoot(),
   ],
