e70b68a2f854b77a8fdfabd7109424f81425d0b9
AiverAiva <43096905+AiverAiva@users.noreply.github.com>
2024-05-14 14:42:59 +0800

    init 2.0

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    UsersModule,
+    AuthModule,
     MongooseModule.forRoot(
-      process.env.MONGODB_URI,
+      'mongodb+srv://leduc1901:melody19@kylecluster.abjzu.mongodb.net/?retryWrites=true&w=majority',
     ),
-    ConfigModule.forRoot({
-      envFilePath: '.env',
-    }),
+    ConfigModule.forRoot(),
   ],
   controllers: [AppController],
   providers: [AppService],
