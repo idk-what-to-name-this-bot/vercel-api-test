97f6cd21ae8e36aa82b5ec8cd338ae98e4e88ec4
AiverAiva <43096905+AiverAiva@users.noreply.github.com>
2024-05-14 15:06:22 +0800

    fix: cant connect to the database

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,10 +10,10 @@ import { ConfigModule } from '@nestjs/config';
   imports: [
     UsersModule,
     AuthModule,
-    MongooseModule.forRoot(
-      'mongodb+srv://leduc1901:melody19@kylecluster.abjzu.mongodb.net/?retryWrites=true&w=majority',
-    ),
     ConfigModule.forRoot(),
+    MongooseModule.forRoot(
+      process.env.MONGODB_URI,
+    ),
   ],
   controllers: [AppController],
   providers: [AppService],
