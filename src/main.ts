95e52317f96c635d29d8b5a9cbbfca26640d9159
AiverAiva <43096905+AiverAiva@users.noreply.github.com>
2024-05-14 13:25:50 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
-  app.setGlobalPrefix('api');
+  // app.setGlobalPrefix('api');
   await app.listen(5019);
 }
 bootstrap();
