d0802b01440ca9896a5e6754f11db60630937e9f
AiverAiva <43096905+AiverAiva@users.noreply.github.com>
2024-05-14 13:08:11 +0800

    qwq

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,14 +8,41 @@ import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+    }),
     UsersModule,
     AuthModule,
     MongooseModule.forRoot(
-      'mongodb+srv://leduc1901:melody19@kylecluster.abjzu.mongodb.net/?retryWrites=true&w=majority',
+      process.env.MONGODB_HOST,
     ),
-    ConfigModule.forRoot(),
+    // ConfigModule.forRoot(),
   ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+
+// @Module({
+//   imports: [
+//     PassportModule.register({ session: true }),
+//     TypeOrmModule.forRoot({
+//       type: 'mongodb',
+//       host: process.env.MYSQL_DB_HOST,
+//       port: 3306,
+//       username: process.env.MYSQL_DB_USERNAME,
+//       password: process.env.MYSQL_DB_PASSWORD,
+//       database: process.env.MYSQL_DB_DATABASE,
+//       synchronize: true,
+//       entities: entities,
+//     }),
+//     AuthModule,
+//     UserModule,
+//     DiscordModule,
+//     GuildsModule,
+//     WebSocketModule,
+//   ],
+//   controllers: [],
+//   providers: [],
+// })
+// export class AppModule {}
